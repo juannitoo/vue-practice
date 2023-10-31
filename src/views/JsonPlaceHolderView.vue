67d40078f714b50c21853966233e99bd82ccf2f3
Jean Balangue <jeanbalangue@hotmail.fr>
2023-10-31 11:16:05 +0100

    jsonplaceholder refacto ModalItem.vue

--- src/views/JsonPlaceHolderView.vue
+++ src/views/JsonPlaceHolderView.vue
@@ -2,10 +2,10 @@
 import { onMounted, ref, computed } from 'vue';
 import LoadingIcon from '../components/LoadingIcon.vue';
 import JsonplaceholderForm from '../components/jsonplaceholder/JsonplaceholderForm.vue';
+import ModalItem from '../components/jsonplaceholder/ModalItem.vue';
 
 const posts = ref();
 const loading = ref(Boolean);
-const modal = ref(false);
 const selectedPost = ref([]);
 const nombrePosts = computed(() => {
   return selectedPost.value.length;
@@ -36,10 +36,6 @@ function toggleSelection(article, button) {
     selectedPost.value.push(article);
     button.textContent = 'Déselectionner';
   }
-}
-
-function toggleModal() {
-  modal.value = !modal.value;
 }
 
 function addPost(data) {
@@ -74,24 +70,11 @@ function addPost(data) {
       </button>
     </article>
 
-    <button class="modale-btn" @click="toggleModal" v-if="!modal">
-      {{ nombrePosts }} article{{ nombrePosts > 1 ? 's' : '' }} sélectionné{{
-        nombrePosts > 1 ? 's' : ''
-      }}
-    </button>
-    <button class="modale-btn" @click="toggleModal" v-else>Fermer la fenêtre</button>
-    <div class="modale" v-if="modal">
-      <h3>Articles sélectionnés</h3>
-      <a
-        v-for="post in selectedPost"
-        :key="post.id"
-        class="selected-articles"
-        :href="`https://jsonplaceholder.typicode.com/posts/${post.id}`"
-        target="_blank"
-      >
-        {{ post.title }}
-      </a>
-    </div>
+    <ModalItem 
+      :nombrePosts="nombrePosts" 
+      :selectedPost="selectedPost"
+    />
+
   </section>
 </template>
 
@@ -161,41 +144,7 @@ p {
     background-color: rgb(210, 186, 6);
   }
 }
-.modale-btn {
-  position: fixed;
-  bottom: 0;
-  right: 0;
-  background-color: rgb(100, 100, 100);
-  padding: 1rem;
-  font-size: 1.5rem;
-  cursor: pointer;
-  color: white;
-  z-index: 2;
-  &:hover {
-    background-color: rgb(50, 50, 50);
-  }
-}
-.modale {
-  position: fixed;
-  bottom: 0rem;
-  height: 50vh;
-  right: 0vw;
-  width: 50%;
-  max-width: 600px;
-  background-color: rgb(100, 100, 100);
-  padding: 1rem;
-  z-index: 1;
-  overflow: auto;
-}
-.modale h3 {
-  text-align: center;
-  font-size: 1.5rem;
-  margin-top: -1rem;
-}
-.modale a {
-  color: white;
-  display: block;
-}
+
 @media (min-width: 850px) {
   #form {
     width: 100%;
