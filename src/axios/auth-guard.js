756f5cdcbdd51e640e8deea0558f787ee52f7e92
Jean Balangue <jeanbalangue@hotmail.fr>
2023-11-15 19:33:52 +0100

    adding dist subtree

--- src/axios/auth-guard.js
+++
@@ -1,9 +0,0 @@
-import router from '@/router'
-import { useUserStore } from '../stores/UserStore.js';
-
-export function authGuard(to){
-  if (useUserStore().isAuthenticated){
-    return true
-  }
-  router.push('/connexion')
-}
